c8c98db71623e9723ca88c14b7914580eb891915
Juvenal PENGELE <juvenal.pengele@gmail.com>
2025-01-25 10:22:27 -0500

    Change the setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: ['@testing-library/jest-dom'],
-  },
+  
 });
